8638393bcbb3d45fa3873cb5e0a5468b3cb824e4
Eduardo Castro <eduardo.alencar.castro@gmail.com>
2024-05-22 12:33:43 -0300

    build: add Howler

--- src/app/details/details.page.ts
+++ src/app/details/details.page.ts
@@ -1,25 +1,23 @@
 import { Component, Input, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonCardHeader, IonCardTitle, IonThumbnail, IonItem, IonBadge, IonButton, IonRow } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonCardHeader, IonCardTitle, IonThumbnail, IonItem, IonBadge, IonButton, IonRow, IonIcon } from '@ionic/angular/standalone';
 import { PokemonService } from '../services/pokemon.service';
 import { PokemonDetails } from '../interfaces/pokemonDetails';
-
+import { Howl } from 'howler'
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.page.html',
   styleUrls: ['./details.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonCardHeader, IonCardTitle, IonThumbnail, IonItem, IonBadge, IonButton, IonRow]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonCardHeader, IonCardTitle, IonThumbnail, IonItem, IonBadge, IonButton, IonRow, IonIcon]
 })
 export class DetailsPage {
   private pokemonService = inject(PokemonService);
   public pokemonData: PokemonDetails | null = null
   public pokemonImageBaseUrl = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/'
   public shiny = false;
-
-
 
   @Input()
   set id(pokemonId: string) {
@@ -30,7 +28,17 @@ export class DetailsPage {
     })
   }
 
+  public player: Howl | null = null;
+
   constructor() { }
+
+
+  handlePlay() {
+    this.player = new Howl({
+      src: this.pokemonData?.cries.latest as string
+    });
+    this.player.play();
+  }
 
   handleShiny() {
     this.shiny = !this.shiny
